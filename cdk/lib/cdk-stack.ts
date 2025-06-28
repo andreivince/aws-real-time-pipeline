48752a41c67712b088ecb53e6b9e499a6a34c861
Andrei Vince <andreivince21@gmail.com>
2025-06-27 20:59:18 -0400

    Organized, Removed Node_modules + New ReadMe

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -6,7 +6,7 @@ import * as path from 'path'
 import * as apigateway from "aws-cdk-lib/aws-apigateway"
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
-export class CdkStack extends cdk.Stack {
+export class MarketStream extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     
